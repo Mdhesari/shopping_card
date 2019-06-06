681d5bf7596f0e594f27b2d640576d3efc7f0504
mohammad fazel <mdhesari99@gmail.com>
2019-06-07 03:19:31 +0430

    add first component the counter

---
+++ src/Components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello my wrold.</h1>
+        <button>test</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
