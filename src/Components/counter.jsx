88e76e003ecf6acf84cc2e867a2448c0e5408ae6
mohammad fazel <mdhesari99@gmail.com>
2019-06-11 02:23:32 +0430

    practise on attributes and conditional rendering

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    tags: []
+  };
+
+  rednerTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+    return this.state.tags.map(tag => <li key={tag}>{tag}</li>);
+  }
+
+  styles = {
+    color: "#fff"
+  };
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hello my wrold.</h1>
-        <button>test</button>
+        <span className="badge badge-primary m-1">{this.formatCount()}</span>
+        <button style={this.styles} className="btn btn-secondary">
+          Increment
+        </button>
+        <ul>
+        {this.state.tags.length === 0 && "Please add new tags."}
+        {this.rednerTags()}</ul>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count == 0 ? "Zero" : count;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Counter from './Components/counter';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Counter/>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
