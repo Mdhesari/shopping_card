2059780973d24fe38974025086e34a7bb95e43bf
mohammad fazel <mdhesari99@gmail.com>
2019-06-14 16:07:24 +0430

    implement reset button the wrong way

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
-    tags: []
+    value: this.props.counters.value,
+    tags: [],
+    handleButtonClass:'btn btn-secondary btn-sm m-2'
   };
 
-  rednerTags() {
+  renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags</p>;
 
     return this.state.tags.map(tag => <li key={tag}>{tag}</li>);
@@ -21,19 +22,35 @@ class Counter extends Component {
     this.setState({ value: this.state.value + 1 });
   };
 
+  handleDecrement = () => {
+    if (this.state.value !== 0) this.setState({ value: this.state.value - 1 });
+  };
+
   printOnConsole(text) {
     console.log(text);
   }
 
+  getBadgeClasses() {
+    let classValue = "badge badge-";
+    const { value } = this.state;
+    return value === 0 ? (classValue += "warning") : (classValue += "primary");
+  }
+
   render() {
-    console.log(this.props);
     return (
       <React.Fragment>
-        <span className="badge badge-primary m-1">{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary">
-          Increment
-        </button>
-        <br />
+        <h4 className="m-2">Title #{this.props.counters.id}</h4>
+        <div className="p-2">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          <button
+            onClick={this.handleIncrement}
+            className={this.state.handleButtonClass}
+          >
+            +
+          </button>
+          <button onClick={this.handleDecrement} className={this.state.handleButtonClass}>-</button>
+          <button onClick={()=>this.props.onDelete(this.props.counters.id)} className="btn btn-danger">Delete</button>
+        </div>
       </React.Fragment>
     );
   }
--- src/Components/counters.jsx
+++ src/Components/counters.jsx
@@ -4,20 +4,55 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     value: [
-        { id: 1, value: 2},
-        { id: 2, value: 0 },
-        { id: 3, value: 0 },
-        { id: 4, value: 0 }
+      { id: 1, value: 2 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
     ]
   };
+
+  handleDelete = counterId => {
+    const value = this.state.value.filter(i => i.id !== counterId);
+    this.setState({ value });
+  };
+
+  handleReset = ()=> {
+    const value = this.state.value.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ value });
+    console.log(value);
+  }
+
+  checkValues() {
+    if (this.state.value.length === 0) {
+      return (
+        <div className="m-3 mt-4">
+          <strong className="alert alert-info">
+            Please add a new item/Reload the page.{" "}
+            <button className="btn btn-success btn-sm m-2">Add</button>
+          </strong>
+        </div>
+      );
+    }
+  }
 
   render() {
     return (
       <React.Fragment>
+        {this.checkValues()}
         <div>
-          {console.log(this.state)}
+          <button onClick={this.handleReset} className="btn btn-primary">
+            Reset
+          </button>
           {this.state.value.map(counter => (
-            <Counter key={counter.id} value={counter.value} selected />
+            <Counter
+              key={counter.id}
+              counters={counter}
+              onDelete={this.handleDelete}
+              selected
+            />
           ))}
         </div>
       </React.Fragment>
